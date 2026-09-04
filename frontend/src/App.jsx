81d5bc224967daf85b7b52ee0a9c3a03917a544a
krishmora <krish.m1906@gmail.com>
2026-09-04 14:49:47 +0530

    frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div>
+      <h1>🛡️ SafeSphere</h1>
+
+      <p>
+        Understand difficult online interactions
+        and discover safer next steps.
+      </p>
+
+      <button>Start Analysis</button>
+    </div>
+  );
+}
+
+export default App;
